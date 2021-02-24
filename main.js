c5a96e0c0845d13f3cd176e4c1742929d1d8bfc5
tranttxuan <>
2021-02-24 11:10:41 +0100

    finish

--- main.js
+++ main.js
@@ -1,12 +1,47 @@
 // nav icon
 
-const selectElement = function(el){
+const selectElement = function (el) {
     return document.querySelector(el)
 }
 
 let menuToggle = selectElement(".menu-toggle");
 let body = selectElement("body");
 
-menuToggle.addEventListener('click', function(){
+menuToggle.addEventListener('click', function () {
     body.classList.toggle("open")
+})
+
+
+
+/* SCROLL REVEAL  */
+
+window.sr = ScrollReveal();
+
+sr.reveal('.animate-left', {
+    origin: 'left',
+    duration: 1000,
+    distance: '25rem',
+    delay: 300
+});
+
+sr.reveal('.animate-right', {
+    origin: 'right',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+})
+
+
+sr.reveal('.animate-top', {
+    origin: 'top',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+})
+
+sr.reveal('.animate-bottom', {
+    origin: 'bottom',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
 })
